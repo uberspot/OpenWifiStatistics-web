b38f7517ed0bee5aed2fdef71a4ddafc30a639fe
Pavlos <onexemailx@gmail.com>
2012-05-06 22:15:08 +0300

    fixed csv form and added sanitization

--- getresultscsv.php
+++ getresultscsv.php
@@ -1,27 +1,37 @@
 <?php
 
+	function sanitizeString($word){
+   		$word = filter_var(filter_var( trim($word) , FILTER_SANITIZE_SPECIAL_CHARS, FILTER_FLAG_STRIP_LOW), FILTER_SANITIZE_MAGIC_QUOTES) ;
+    		if ((strpos($word,'|')>0) || (strpos($word,'\\')>0) || (strpos($word,'/')>0)
+           		|| (strpos($word,'>')>0) || (strpos($word,'<')>0) || (strpos($word,'\"')>0)|| (strpos($word,'\'')>0)|| (strpos($word,'`')>0)
+           		|| (strpos($word,'~')>0) || (strpos($word,'$')>0) || (strpos($word,'\"')>0)
+           		|| (strpos($word,'\'')>0)|| (strpos($word,'`')>0)) {
+			$word = "";
+		}
+		return $word;
+	}
 	require_once('statsModel.php');
 	require_once('statModel.php');
 
 
 	if(isset($_GET['longtitude']) && isset($_GET['latitude'])) {
-		$long = $_GET['longtitude'];
-		$lat = $_GET['latitude'];
+		$long = sanitizeString($_GET['longtitude']);
+		$lat = sanitizeString($_GET['latitude']);
 		
-		$km = 50;
-		$km = $km*0.005;
+		if(empty($long) || empty($lat)) { 
+			echo "Invalid parameters!";
+			exit; 
+		}
+
+		$km = 50*0.005;
 		
 		$results = new statsModel();
 		
-		$first = true;
+		echo "bssid;ssid;security;latitude;longitude\n";
 		foreach($results->getResultsByLocation($long,$lat,$km) as $result) {
-			if(!$first)
-				echo ',';
-			else
-				$first = false;
-			echo $result->getSsid().','.$result->getLongitude().','.$result->getLatitude();
+			echo $result->getBssid().';'.$result->getSsid().';'.$result->getCapabilities().';'.$result->getLongitude().';'.$result->getLatitude()."\n";
 		}		
 	} else {
-		echo "ERROR";
+		echo "No parameters set!";
 	}
 ?>
